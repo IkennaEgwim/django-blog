3ec480ecabcae7177bccd226fa11a44712a54351
Ikenna Egwim <egwimik@yahoo.com>
2024-06-18 12:05:34 +0000

    Add a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dhxr00%qws$%2$32%32whv1_2gfabmgy257^05cbkh)2kjr)&+'
+# SECRET_KEY = 'django-insecure-dhxr00%qws$%2$32%32whv1_2gfabmgy257^05cbkh)2kjr)&+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +90,12 @@ DATABASES = {
     'default': 
 dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com", 
+    "https://8000-ikennaegwim-djangoblog-xz4irh9i2kb.ws-eu114.gitpod.io",
+]
 
 
 # Password validation
